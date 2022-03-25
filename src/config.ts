1e14c6e01440e6b47daff217aabc3e87b1e9e60c
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2022-03-25 16:14:43 +0000

    Migrate to ts

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import Phaser from 'phaser';
+
+var _w = window.innerWidth, _h = window.innerHeight;
+
+export default {
+  type: Phaser.AUTO,
+  width: _w,
+  height: _h,
+  physics: {
+    default: 'matter',
+    matter: {
+      gravity: { 
+        y: 100 
+      }
+    }
+  },
+};
